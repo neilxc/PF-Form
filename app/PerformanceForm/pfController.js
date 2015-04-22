5539aa9ab095dd00f4216c8f264c3360c4748066
neilxc <neilxc@gmail.com>
2015-04-22 16:54:15 +0100

    end work

--- app/PerformanceForm/pfController.js
+++ app/PerformanceForm/pfController.js
@@ -1,4 +1,49 @@
 myApp.controller('pfController',
-    function pfController($scope,pfService){
+    function pfController($scope,pfService, $modal) {
         $scope.employee = pfService.employee;
+
     });
+
+myApp.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
+
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+myApp.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+    $scope.items = items;
+    $scope.selected = {
+        item: $scope.items[0]
+    };
+
+    $scope.ok = function () {
+        $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
